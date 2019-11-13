dbaf73b3a3454eec1bb517ed8533598292b73240
kulkarni41 <48361314+kulkarni41@users.noreply.github.com>
2019-11-13 10:47:31 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,22 @@
 import json
+import boto3
+import decimal
 import requests
-y = {"name": "Swapnil"}
-#print(str(payload))
-print(y["name"])
-response = requests.get("https://x07kfctuuh.execute-api.us-east-2.amazonaws.com/Stage")
-print(response.status_code)
-print(response.json())
+def handler(event, context):
+    for record in event['Records']:
+       payload=record["body"]
+       y = json.loads(payload)
+       #print(str(payload))
+	     response = requests.get("https://x07kfctuuh.execute-api.us-east-2.amazonaws.com/Stage")
+	     x = (response.json())
+	     uid = (x["vid"])
+       num = (y["number"])
+       dynamodb = boto3.resource('dynamodb')
+       dynamoTable = dynamodb.Table('test_demo')
+       dynamoTable.put_item(
+          Item={
+              'user_id': uid,
+              'contact_number': num
+              }
+          )
+       print("PutItem succeeded:")
